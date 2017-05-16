7c1735ac9993c2cb5efe812750183a906f70098d
Dmytro Polishchuk <d.polischyk@gmail.com>
2017-05-16 06:27:56 +0300

    Initial Commit

---
+++ Translator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Translator
+//
+//  Created by Dmytro Polishchuk on 5/16/17.
+//  Copyright © 2017 Dmytro Polishchuk. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
