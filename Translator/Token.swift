676e52c0085afaf6ccd13f25c73bafb4251a8031
Dmytro Polishchuk <d.polischyk@gmail.com>
2017-05-18 23:26:18 +0300

    Replaced TextPoint to separate file

--- Translator/Token.swift
+++ Translator/Token.swift
@@ -9,35 +9,6 @@
 import Foundation
 
 
-/// Represents position in text
-struct TextPoint: Hashable, Equatable, CustomStringConvertible {
-    
-    let line: Int
-    let character: Int
-    
-    
-    // MARK: - CustomStringConvertible
-    
-    var description: String {
-        return "\(line):\(character)"
-    }
-    
-    
-    // MARK: - Hashable
-    
-    var hashValue: Int {
-        return line ^ character
-    }
-    
-    
-    // MARK: - Equatable
-    
-    static func ==(lhs: TextPoint, rhs: TextPoint) -> Bool {
-        return lhs.line == rhs.line && lhs.character == rhs.character
-    }
-}
-
-
 /// Represents token
 class Token: Hashable, Equatable, CustomStringConvertible {
     
