27972eaf6804484bb8ee9155db56473773e037a5
Dmytro Polishchuk <d.polischyk@gmail.com>
2017-05-30 02:58:52 +0300

    Added RPNLabelKeeper that keeps label inside.

---
+++ Translator/RPNLabelKeeper.swift
@@ -0,0 +1,18 @@
+//
+//  RPNLabelKeeper.swift
+//  Translator
+//
+//  Created by Dmytro Polishchuk on 5/29/17.
+//  Copyright © 2017 Dmytro Polishchuk. All rights reserved.
+//
+
+import Foundation
+
+class RPNLabelKeeper: ComplexPolishOperator {
+    lazy var labels = [Label]()
+    
+    override var stringValue: String {
+        let labelsStingValue = labels.reduce("") { result, next in "\(result)\(next)" }
+        return super.stringValue + labelsStingValue
+    }
+}
