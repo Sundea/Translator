2ea83a223eaf4ea0f9df880ac8cf1d2d65652e6a
Dmytro Polishchuk <d.polischyk@gmail.com>
2017-05-29 20:10:50 +0300

    Added Reverse Polish Element protocol, represents element of RPN translation.

---
+++ Translator/ReversePolishElement.swift
@@ -0,0 +1,15 @@
+//
+//  ReversePolishElement.swift
+//  Translator
+//
+//  Created by Dmytro Polishchuk on 5/29/17.
+//  Copyright © 2017 Dmytro Polishchuk. All rights reserved.
+//
+
+import Foundation
+
+protocol ReversePolishElement {
+    
+    /// Human-readable representation of reverse polish element
+    var representation: String { get }
+}
