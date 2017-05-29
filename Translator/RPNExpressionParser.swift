92f25b51bf313c531f91da4ce50c9d399c1aea86
Dmytro Polishchuk <d.polischyk@gmail.com>
2017-05-29 22:57:26 +0300

    Added PRNExpression parser, RPNParser subclass. Parses arifmethic expressions.

---
+++ Translator/RPNExpressionParser.swift
@@ -0,0 +1,32 @@
+//
+//  RPNExpressionParser.swift
+//  Translator
+//
+//  Created by Dmytro Polishchuk on 5/29/17.
+//  Copyright © 2017 Dmytro Polishchuk. All rights reserved.
+//
+
+import Foundation
+
+class RPNExpressionParser: RPNParser {
+    
+    override func replace(_ operation: SimplePolishOperator, at position: TextPoint) -> SimplePolishOperator {
+        var result = operation
+        
+        if operation === OperatorPool.minus, let top = magazine.top, top == OperatorPool.assignValue {
+            result = OperatorPool.unaryMinus
+        }
+        
+        return result
+    }
+    
+    override func customAction(_ operation: SimplePolishOperator) -> Bool {
+        if operation === OperatorPool.closeParenthesis {
+            while let pop = magazine.pop(), pop != OperatorPool.openParenthesis {
+                output.append(pop)
+            }
+            return true
+        }
+        return false
+    }
+}
